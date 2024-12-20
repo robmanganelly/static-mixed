00a1d90d2da48501a5d8bacf6c3e1392fbe3e548
Roberto Manganelly <robmanganelly@gmail.com>
2024-12-20 00:19:37 -0500

    adding app initial

--- projects/apps/rent-buy-calc/jest.config.ts
+++ projects/apps/rent-buy-calc/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'rent-buy-calc',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/rent-buy-calc',
+  coverageDirectory: '../../../coverage/projects/apps/rent-buy-calc',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
